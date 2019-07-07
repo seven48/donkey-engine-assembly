c0bc1dae5a5acc07d70cf61e43ccbbf2a24b00f4
Simon Suprun <simonsuprun@gmail.com>
2019-07-07 17:12:42 +0300

    Add server startup

---
+++ app.py
@@ -0,0 +1,26 @@
+""" Main module of the application """
+
+from asyncio import get_event_loop
+
+from src.connection import Connection
+from src.settings import (
+    ASSEMBLY_MQ_URL,
+    ASSEMBLY_QUEUE_NAME
+)
+
+
+async def main() -> None:
+    """ Main function """
+
+    connection = Connection(
+        url=ASSEMBLY_MQ_URL,
+        queue_name=ASSEMBLY_QUEUE_NAME
+    )
+    async with connection:
+        await connection.get()
+
+
+if __name__ == "__main__":
+    LOOP = get_event_loop()
+    LOOP.run_until_complete(main())
+    LOOP.close()

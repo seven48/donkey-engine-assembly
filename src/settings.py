50f5bd3470ce8f95b3d363a37a9a81ae5aeece9a
Simon Suprun <simonsuprun@gmail.com>
2019-07-07 17:12:19 +0300

    Add settings module for getting params from environment variables

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+""" Project settinsg """
+
+import os
+
+
+ASSEMBLY_MQ_URL: str = (
+    os.getenv('ASSEMBLY_MQ_URL') or 'amqp://guest:guest@127.0.0.1/'
+)
+ASSEMBLY_QUEUE_NAME: str = os.getenv('ASSEMBLY_QUEUE_NAME') or 'assembly'

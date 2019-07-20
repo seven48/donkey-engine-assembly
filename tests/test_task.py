468b6b88064bb1553fc7c0743b3b4f5df752c094
Simon Suprun <simonsuprun@gmail.com>
2019-07-21 02:24:17 +0300

    Write tests for task delegating

---
+++ tests/test_task.py
@@ -0,0 +1,45 @@
+"""Module for testing Task."""
+
+from json import dumps
+
+import pytest
+
+from src.assets import workers
+from src.task import Task
+
+success_message_body = {
+    'game': 'Minecraft: Java Edition',
+    'version': '1.2.5',
+    'mods': [
+        'CraftBukkit',
+    ],
+    'configs': {
+        'max-players': 20,
+    },
+}
+
+
+class MockIncommingMessage(object):
+    """Mock class of pika IncommingMessage."""
+
+    def __init__(self, text):
+        """Initializate mock object."""
+        self.body = dumps(text).encode()
+
+
+@pytest.mark.asyncio
+async def test_task_success():
+    """Test Task class with success result."""
+    message = MockIncommingMessage(success_message_body)
+
+    task = Task(message)
+    assert task.text == message.body.decode('UTF-8')
+    assert task.json == success_message_body
+
+    worker = task.delegate()
+    minecraft_builder = workers.get('Minecraft: Java Edition')
+    assert isinstance(worker, minecraft_builder)
+
+    assert task.json['version'] == worker.version
+    assert task.json['mods'] == worker.mods
+    assert task.json['configs'] == worker.configs
